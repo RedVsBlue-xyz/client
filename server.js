672031612408575f0347e4f448b7bdd0129f3e25
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-12-14 21:27:44 -0600

    push notification poll

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const webpush = require('web-push');
 const bodyParser = require('body-parser');
 const { parse } = require('url');
 const next = require('next');
+const { createPublicClient, http } = require('viem')
+const { mainnet } = require('viem/chains')
+
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+})
+
+client.getBlockNumber().then(blockNumber => {
+  console.log("blockNumber", blockNumber)
+})
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
@@ -18,6 +29,17 @@ webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey)
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
+let subscriptions = [];
+
+//send push notification every 30 seconds
+setInterval(() => {
+  const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
+  subscriptions.forEach(subscription => {
+    webpush.sendNotification(subscription, payload).catch(console.log);
+  } );
+} , 30000);
+
+
 app.prepare().then(() => {
     const server = express();
 
@@ -25,6 +47,7 @@ app.prepare().then(() => {
 
     server.post('/subscribe', (req, res) => {
       const subscription = req.body;
+      subscriptions.push(subscription);
       res.status(201).json({});
       const payload = JSON.stringify({ title: "Hello World", body: "This is your first push notification" });
       console.log(subscription);
